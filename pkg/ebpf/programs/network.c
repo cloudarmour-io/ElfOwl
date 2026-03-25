a58da594669ae80c3eab010a91a5ebc49c9cb382
Tirveni Yadav <tirveni@udyansh.org>
2026-03-25 11:27:59 +0530

    feat: extend network probe with IPv6 and UDP

--- pkg/ebpf/programs/network.c
+++ pkg/ebpf/programs/network.c
@@ -1,5 +1,5 @@
 // ANCHOR: Network Monitor eBPF Program - Mar 25, 2026
-// Restores TCP tuple fidelity via tracepoint/tcp/tcp_connect.
+// Adds TCP state, UDP send tracing, IPv6 tuples, and netns metadata.
 
 #include "common.h"
 
@@ -11,13 +11,36 @@ struct network_event {
 	__u16 dport;
 	__u32 saddr;
 	__u32 daddr;
+	__u8 saddr_v6[16];
+	__u8 daddr_v6[16];
 	__u8 protocol;
+	__u8 direction;
+	__u8 state;
+	__u32 netns;
 	__u64 cgroup_id;
 } __attribute__((packed));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
 } network_events SEC(".maps");
+
+#define NET_DIR_UNKNOWN 0
+#define NET_DIR_OUTBOUND 1
+#define NET_DIR_INBOUND 2
+
+struct sys_enter_sendto_ctx {
+	__u16 common_type;
+	__u8 common_flags;
+	__u8 common_preempt_count;
+	__s32 common_pid;
+	long __syscall_nr;
+	long fd;
+	const char *buff;
+	long len;
+	long flags;
+	const struct sockaddr *addr;
+	long addr_len;
+};
 
 static __always_inline __u32 pack_ipv4(const __u8 addr[4])
 {
@@ -27,12 +50,64 @@ static __always_inline __u32 pack_ipv4(const __u8 addr[4])
 	       ((__u32)addr[3] << 24);
 }
 
+static __always_inline int read_udp_destination(const struct sockaddr *uaddr, __u32 addr_len,
+				    __u16 *family, __u16 *dport, __u32 *daddr, __u8 daddr_v6[16])
+{
+	struct sockaddr_in dst4 = {};
+	struct sockaddr_in6 dst6 = {};
+
+	if (!uaddr || addr_len < sizeof(dst4)) {
+		return -1;
+	}
+
+	if (bpf_probe_read_user(&dst4, sizeof(dst4), uaddr) < 0) {
+		return -1;
+	}
+
+	if (dst4.sin_family == AF_INET) {
+		*family = dst4.sin_family;
+		*dport = dst4.sin_port;
+		*daddr = dst4.sin_addr.s_addr;
+		return 0;
+	}
+
+	if (addr_len < sizeof(dst6)) {
+		return -1;
+	}
+
+	if (bpf_probe_read_user(&dst6, sizeof(dst6), uaddr) < 0) {
+		return -1;
+	}
+
+	if (dst6.sin6_family != AF_INET6) {
+		return -1;
+	}
+
+	*family = dst6.sin6_family;
+	*dport = dst6.sin6_port;
+	__builtin_memcpy(daddr_v6, &dst6.sin6_addr, sizeof(dst6.sin6_addr));
+	return 0;
+}
+
+static __always_inline __u8 infer_direction(int oldstate, int newstate)
+{
+	if (newstate == TCP_SYN_SENT) {
+		return NET_DIR_OUTBOUND;
+	}
+	if (newstate == TCP_SYN_RECV || newstate == TCP_NEW_SYN_RECV) {
+		return NET_DIR_INBOUND;
+	}
+	return NET_DIR_UNKNOWN;
+}
+
+// ANCHOR: Network state + UDP coverage - Feature: advanced network telemetry - Mar 25, 2026
+// Emits connection tuples from tcp_connect, inet_sock_set_state, and UDP sendto.
 SEC("tracepoint/tcp/tcp_connect")
 int network_monitor(struct trace_event_raw_tcp_event_sk *ctx)
 {
 	struct network_event evt = {};
 
-	if (ctx->family != AF_INET) {
+	if (ctx->family != AF_INET && ctx->family != AF_INET6) {
 		return 0;
 	}
 
@@ -40,10 +115,80 @@ int network_monitor(struct trace_event_raw_tcp_event_sk *ctx)
 	evt.family = ctx->family;
 	evt.sport = ctx->sport;
 	evt.dport = ctx->dport;
-	evt.saddr = pack_ipv4(ctx->saddr);
-	evt.daddr = pack_ipv4(ctx->daddr);
 	evt.protocol = IPPROTO_TCP;
+	evt.direction = NET_DIR_OUTBOUND;
+	evt.state = 0;
+	evt.netns = current_netns_inum();
 	evt.cgroup_id = bpf_get_current_cgroup_id();
+
+	if (ctx->family == AF_INET) {
+		evt.saddr = pack_ipv4(ctx->saddr);
+		evt.daddr = pack_ipv4(ctx->daddr);
+	} else {
+		__builtin_memcpy(evt.saddr_v6, ctx->saddr_v6, sizeof(evt.saddr_v6));
+		__builtin_memcpy(evt.daddr_v6, ctx->daddr_v6, sizeof(evt.daddr_v6));
+	}
+
+	SUBMIT_EVENT(ctx, network_events, &evt);
+	return 0;
+}
+
+SEC("tracepoint/inet_sock/inet_sock_set_state")
+int network_state_monitor(struct trace_event_raw_inet_sock_set_state *ctx)
+{
+	struct network_event evt = {};
+
+	if (ctx->family != AF_INET && ctx->family != AF_INET6) {
+		return 0;
+	}
+
+	evt.pid = current_pid();
+	evt.family = ctx->family;
+	evt.sport = ctx->sport;
+	evt.dport = ctx->dport;
+	evt.protocol = (__u8)ctx->protocol;
+	evt.direction = infer_direction(ctx->oldstate, ctx->newstate);
+	evt.state = (__u8)ctx->newstate;
+	evt.netns = current_netns_inum();
+	evt.cgroup_id = bpf_get_current_cgroup_id();
+
+	if (ctx->family == AF_INET) {
+		evt.saddr = pack_ipv4(ctx->saddr);
+		evt.daddr = pack_ipv4(ctx->daddr);
+	} else {
+		__builtin_memcpy(evt.saddr_v6, ctx->saddr_v6, sizeof(evt.saddr_v6));
+		__builtin_memcpy(evt.daddr_v6, ctx->daddr_v6, sizeof(evt.daddr_v6));
+	}
+
+	SUBMIT_EVENT(ctx, network_events, &evt);
+	return 0;
+}
+
+SEC("tracepoint/syscalls/sys_enter_sendto")
+int network_udp_send(struct sys_enter_sendto_ctx *ctx)
+{
+	struct network_event evt = {};
+	__u16 family = 0;
+	__u16 dport = 0;
+	__u32 daddr = 0;
+	__u8 daddr_v6[16] = {};
+
+	if (read_udp_destination(ctx->addr, (__u32)ctx->addr_len, &family, &dport, &daddr, daddr_v6) < 0) {
+		return 0;
+	}
+
+	evt.pid = current_pid();
+	evt.family = family;
+	evt.sport = 0;
+	evt.dport = dport;
+	evt.saddr = 0;
+	evt.daddr = daddr;
+	evt.protocol = IPPROTO_UDP;
+	evt.direction = NET_DIR_OUTBOUND;
+	evt.state = 0;
+	evt.netns = current_netns_inum();
+	evt.cgroup_id = bpf_get_current_cgroup_id();
+	__builtin_memcpy(evt.daddr_v6, daddr_v6, sizeof(evt.daddr_v6));
 
 	SUBMIT_EVENT(ctx, network_events, &evt);
 	return 0;
